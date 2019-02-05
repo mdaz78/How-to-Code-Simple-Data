ee9acf0f1a5802477767b51f370a741eeb003115
Md Abu Zafar <mdabuzafar78@gmail.com>
2019-02-05 09:16:51 +0530

    Learn basics of bsl

---
+++ 1a: Beginning Student Language/string.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname string) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(string-append "Ada"  " " "Lovelace")
+(string-length "apple")
+(substring "01234567" 0 2)
